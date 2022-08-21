51c090c743784cf23028bf8f96e1e5151ceb3421
n0izzzz <tarasov.dima@gmail.com>
2022-08-21 16:32:44 +0300

    8.19. Правда или действие (часть 1) / 1

--- js/form.js
+++ js/form.js
@@ -1,14 +1,14 @@
-const adForm = document.querySelector('.ad-form');
-
 const FormStates = {
   Enable: true,
   Disable: false,
 };
 
+const adForm = document.querySelector('.ad-form');
+const sliderPrice = document.querySelector('.ad-form__slider');
+const updateSliderPrice = document.querySelector('input#price');
+
 const changeFormState = (state) => {
-
   const allDisablingItems = document.querySelectorAll('fieldset, fieldset, .map__filters select');
-
   if (state === FormStates.Disable) {
     adForm.classList.add('ad-form--disabled');
   }
@@ -18,5 +18,64 @@ const changeFormState = (state) => {
     }
   });
 };
+
+noUiSlider.create(sliderPrice, {
+  range: {
+    'min': 1000,
+    'max': 100000
+  },
+  start: 0,
+  margin: 2,
+  step: 1000,
+});
+
+sliderPrice.noUiSlider.on('update', (values, handle) => {
+  updateSliderPrice.value = values[handle];
+});
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error'
+});
+
+const capacityOptions = {
+  '1 комната': ['для 1 гостя'],
+  '2 комнаты': ['для 2 гостей', 'для 1 гостя'],
+  '3 комнаты': ['для 3 гостей', 'для 2 гостей', 'для 1 гостя'],
+  '100 комнат': ['не для гостей']
+};
+
+const roomNumber = adForm.querySelector('[name="rooms"]');
+const capacity = adForm.querySelector('[name="capacity"]');
+
+function validateCapacity() {
+  return capacityOptions[roomNumber.value].includes(capacity.value);
+}
+
+
+const errorText = () => {
+  if (roomNumber.value === '1 комната') {
+    return `${roomNumber.value} подходит только для 1 гостя`;
+  } if (roomNumber.value === '2 комнаты') {
+    return `${roomNumber.value} подходят для только 1 или 2 гостей`;
+  } if (roomNumber.value === '3 комнаты') {
+    return `${roomNumber.value} подходят для только 1, 2 или 3 гостей`;
+  } else {
+    return `${roomNumber.value} только не для гостей`;
+  }
+};
+
+pristine.addValidator(roomNumber, validateCapacity, errorText);
+pristine.addValidator(capacity, validateCapacity, errorText);
+
+
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
 
 export { changeFormState, FormStates };
--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ import {getPopup} from './generate-similar-objects.js';
 import {changeFormState, FormStates} from './form.js';
 import { getSimilarAds } from './data.js';
 
-changeFormState(FormStates.Disable);
+changeFormState(FormStates.Enable);
 
 getPopup(getSimilarAds()[1]);
