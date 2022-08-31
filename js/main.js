25424db652c7d69ff60c211447fce5eeb8a649b8
n0izzzz <tarasov.dima@gmail.com>
2022-08-31 22:29:50 +0300

    11.15. Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-import { getSimilarAds } from './data.js';
-import {initMap} from './map.js';
+//import { getSimilarAds } from './data.js';
+//import {initMap} from './map.js';
+import {getData} from './api.js';
 
-const ads = getSimilarAds();
-initMap(ads);
+//const ads = getSimilarAds();
+//initMap(ads);
+getData();
